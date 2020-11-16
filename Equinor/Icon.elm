c66965f891f07fdfaf34468c374a42a6b7d8eb4d
Atle Wee Førre <ATFO@equinor.com>
2020-11-16 11:06:26 +0100

    Add excel and pdf icons

--- Equinor/Icon.elm
+++ Equinor/Icon.elm
@@ -1,4 +1,4 @@
-module Equinor.Icon exposing (applications, arrowTop, arrow_down, arrow_left, arrow_right, arrow_up, bulletList, cable, camera, cameraFlip, check, circuit, close, curvedArrowTopLeft, document, downArrow, download, ducting, echo, echoLogo, electrical, equinorStar, error, eye, favorite, fireAndGas, flashOff, flashOn, folder, folder_open, function, heatTrace, hierarchy, instrument, junctionBox, keyboardArrowDown, keyboardArrowLeft, keyboardArrowRight, keyboardArrowUp, line_, manualValve, menu, more, open_in_new, pileOfDocuments, refresh, rotate_left, rotate_right, search, settings, signal, tag, telecom, windowsFolder, wrench, zoomIn, zoomOut)
+module Equinor.Icon exposing (applications, arrowTop, arrow_down, arrow_left, arrow_right, arrow_up, bulletList, cable, camera, cameraFlip, check, circuit, close, curvedArrowTopLeft, document, downArrow, download, ducting, echo, echoLogo, electrical, equinorStar, error, excel, eye, favorite, fireAndGas, flashOff, flashOn, folder, folder_open, function, heatTrace, hierarchy, instrument, junctionBox, keyboardArrowDown, keyboardArrowLeft, keyboardArrowRight, keyboardArrowUp, line_, manualValve, menu, more, open_in_new, pdf, pileOfDocuments, refresh, rotate_left, rotate_right, search, settings, signal, tag, telecom, windowsFolder, wrench, zoomIn, zoomOut)
 
 import Html.Attributes as HA
 import Svg exposing (Svg, circle, g, polygon, rect, svg, text, text_)
@@ -665,4 +665,31 @@ windowsFolder =
             , stroke "#FFB900"
             ]
             []
+        ]
+
+
+excel =
+    svg [ viewBox "170 90 220 220" ]
+        [ Svg.path [ fill "#21a366", d "m307.5 103.75h-73.334c-5.028 0-9.167 4.139-9.167 9.166v38.959l82.501 48.125 41.25 17.405 41.25-17.405v-48.125z" ] []
+        , Svg.path [ fill "#107c41", d "m224.999 151.875h82.501v48.125h-82.501z" ] []
+        , Svg.path [ fill "#33c481", d "m380.834 103.75h-73.334v48.125h82.5v-38.959c0-5.027-4.139-9.166-9.166-9.166z" ] []
+        , Svg.path [ fill "#185c37", d "m307.5 200h-82.501v87.084c0 5.027 4.139 9.166 9.167 9.166h146.668c5.027 0 9.166-4.139 9.166-9.166v-38.959z" ] []
+        , Svg.path [ fill "#107c41", d "m307.5 200h82.5v48.125h-82.5z" ] []
+        , g []
+            [ Svg.path [ fillOpacity ".094118", d "m284.584 138.125h-59.584v130.625h59.584c5.022-.014 9.152-4.144 9.166-9.166v-112.293c-.014-5.022-4.144-9.152-9.166-9.166z" ] []
+            , g [ fillOpacity ".2" ]
+                [ Svg.path [ d "m277.709 145h-52.709v130.625h52.709c5.022-.014 9.152-4.144 9.166-9.166v-112.293c-.014-5.022-4.144-9.152-9.166-9.166z" ] []
+                , Svg.path [ d "m277.709 145h-52.709v116.875h52.709c5.022-.014 9.152-4.144 9.166-9.166v-98.543c-.014-5.022-4.144-9.152-9.166-9.166z" ] []
+                , Svg.path [ d "m270.834 145h-45.834v116.875h45.834c5.022-.014 9.152-4.144 9.166-9.166v-98.543c-.014-5.022-4.144-9.152-9.166-9.166z" ] []
+                ]
+            , Svg.path [ fill "#107c41", d "m179.166 145h91.668c5.027 0 9.166 4.139 9.166 9.166v91.668c0 5.027-4.138 9.166-9.166 9.166h-91.668c-5.027 0-9.166-4.139-9.166-9.166v-91.668c0-5.027 4.139-9.166 9.166-9.166z" ] []
+            , Svg.path [ fill "#fff", d "m194.292 234.375 22.246-34.471-20.376-34.279h16.397l11.123 21.909c1.022 2.079 1.725 3.63 2.109 4.652h.144c.734-1.661 1.502-3.276 2.302-4.843l11.889-21.718h15.055l-20.904 34.087 21.431 34.663h-16.014l-12.849-24.067c-.605-1.023-1.118-2.099-1.534-3.212h-.191c-.376 1.091-.874 2.136-1.487 3.115l-13.232 24.164z" ] []
+            ]
+        ]
+
+
+pdf =
+    svg [ viewBox "0 0 128 128" ]
+        [ Svg.path [ fill "#FD4233", d "M104,126H24c-5.514,0-10-4.486-10-10V12c0-5.514,4.486-10,10-10h40.687      c2.671,0,5.183,1.041,7.07,2.929l39.314,39.314c1.889,1.889,2.929,4.399,2.929,7.07V116C114,121.514,109.514,126,104,126z M24,6      c-3.309,0-6,2.691-6,6v104c0,3.309,2.691,6,6,6h80c3.309,0,6-2.691,6-6V51.313c0-1.579-0.641-3.125-1.757-4.242L68.929,7.757      C67.796,6.624,66.289,6,64.687,6H24z" ] []
+        , Svg.path [ fill "#FF402F", d "M95.21,80.32c-0.07-0.51-0.48-1.15-0.92-1.58c-1.26-1.24-4.03-1.89-8.25-1.95      c-2.86-0.03-6.3,0.22-9.92,0.73c-1.62-0.93-3.29-1.95-4.6-3.18c-3.53-3.29-6.47-7.86-8.31-12.89c0.12-0.47,0.22-0.88,0.32-1.3      c0,0,1.98-11.28,1.46-15.1c-0.07-0.52-0.12-0.67-0.26-1.08l-0.17-0.44c-0.54-1.25-1.6-2.57-3.26-2.5L60.32,41H60.3      c-1.86,0-3.36,0.95-3.76,2.36c-1.2,4.44,0.04,11.09,2.29,19.69l-0.58,1.4c-1.61,3.94-3.63,7.9-5.41,11.39l-0.23,0.45      c-1.88,3.67-3.58,6.79-5.13,9.43l-1.59,0.84c-0.12,0.06-2.85,1.51-3.49,1.89c-5.43,3.25-9.03,6.93-9.63,9.85      c-0.19,0.94-0.05,2.13,0.92,2.68l1.54,0.78c0.67,0.33,1.38,0.5,2.1,0.5c3.87,0,8.36-4.82,14.55-15.62      c7.14-2.32,15.28-4.26,22.41-5.32c5.43,3.05,12.11,5.18,16.33,5.18c0.75,0,1.4-0.07,1.92-0.21c0.81-0.22,1.49-0.68,1.91-1.3      C95.27,83.76,95.43,82.06,95.21,80.32z M36.49,99.33c0.7-1.93,3.5-5.75,7.63-9.13c0.26-0.21,0.9-0.81,1.48-1.37      C41.28,95.72,38.39,98.46,36.49,99.33z M60.95,43c1.24,0,1.95,3.13,2.01,6.07c0.06,2.94-0.63,5-1.48,6.53      c-0.71-2.26-1.05-5.82-1.05-8.15C60.43,47.45,60.38,43,60.95,43z M53.65,83.14c0.87-1.55,1.77-3.19,2.69-4.92      c2.25-4.25,3.67-7.57,4.72-10.3c2.1,3.82,4.72,7.07,7.79,9.67c0.39,0.32,0.8,0.65,1.22,0.98C63.82,79.8,58.41,81.31,53.65,83.14      z M93.08,82.79c-0.38,0.23-1.47,0.37-2.17,0.37c-2.26,0-5.07-1.03-9-2.72c1.51-0.11,2.9-0.17,4.14-0.17      c2.27,0,2.94-0.01,5.17,0.56C93.44,81.4,93.47,82.55,93.08,82.79z" ] []
         ]
